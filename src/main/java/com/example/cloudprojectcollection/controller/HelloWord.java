83d80b3ac4da2b28e2462747f78fe2eda97249bb
wangweixiang <LW15239932660@163.com>
2022-08-15 09:28:46 +0800

    多线程异步执行DEMO->CompletableFuture

--- src/main/java/com/example/cloudprojectcollection/controller/HelloWord.java
+++ src/main/java/com/example/cloudprojectcollection/controller/HelloWord.java
@@ -1,11 +1,17 @@
 package com.example.cloudprojectcollection.controller;
 
 import com.example.cloudprojectcollection.service.TestService;
+import io.netty.util.concurrent.CompleteFuture;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * @Author: Wwx
@@ -34,5 +40,76 @@ public class HelloWord {
         return testService.getTree();
     }
 
+    /**
+     * 多线程异步执行DEMO
+     * @return
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @RequestMapping("/getAsync")
+    public List getAsync() throws ExecutionException, InterruptedException {
+        System.out.println(LocalDate.now()+"-"+ LocalTime.now());
+        ExecutorService executor = new ThreadPoolExecutor(4,5,5000l, TimeUnit.SECONDS,new LinkedBlockingDeque<>());
+        List list = new ArrayList();
+        CompletableFuture<Boolean> a = CompletableFuture.supplyAsync(() -> {
+            try {
+                getA(list);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return Boolean.TRUE;
+        },executor);
+
+        CompletableFuture<Boolean> b = CompletableFuture.supplyAsync(() -> {
+            try {
+                getB(list);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return Boolean.TRUE;
+        },executor);
+
+        CompletableFuture<Boolean> c = CompletableFuture.supplyAsync(() -> {
+            try {
+                getC(list);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return Boolean.TRUE;
+        },executor);
+
+        CompletableFuture<Boolean> d = CompletableFuture.supplyAsync(() -> {
+            try {
+                getD(list);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return Boolean.TRUE;
+        },executor);
+
+        a.get();
+        b.get();
+        c.get();
+        d.get();
+        System.out.println(LocalDate.now()+"-"+ LocalTime.now());
+        return list;
+    }
+
+
+    public void getA(List list) throws InterruptedException {
+        list.add("王卫翔");
+    }
+
+    public void getB(List list) throws InterruptedException {
+        list.add("陈鹏元");
+    }
+
+    public void getC(List list) throws InterruptedException {
+        list.add("张延顺");
+    }
+
+    public void getD(List list) throws InterruptedException {
+        list.add("张文亮");
+    }
 }
 
